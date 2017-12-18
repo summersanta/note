aefc413c64df1ba24ee518837105b6c1de9e13ba
Gyeongwon Jo <jessy1085@gmail.com>
2017-12-18 22:19:54 +0900

    Real Calendar Prompt

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -3,26 +3,46 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Prompt {
+	/**
+	 * 
+	 * @param week 요일명
+	 * @return 0~6 (0 = sunday, 6= saturday)
+	 */
 
-	private final static String PROMPT = "cal> ";
-
+public int parseDay(String week) {
+	if (week.equals("su"))
+		return 0;
+	else if(week.equals("mo"))	return 1;
+	else if(week.equals("tu"))	return 2;
+	else if(week.equals("we"))	return 3;
+	else if(week.equals("th"))	return 4;
+	else if(week.equals("fr"))	return 5;
+	else if(week.equals("sa"))	return 6;
+	else
+		return 0;
+}
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar2 cal = new Calendar2();
 
 		int month = 1;
+		int year = 2017;
 
 		while (true) {
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if (month == -1) {
+			System.out.println("년도를 입력하세요. (exit : -1)");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
+			if (year == -1)
 				break;
-			}
-			if (month > 12) {
+			System.out.println("달을 입력하세요.");
+			System.out.print("MONTH> ");
+			month = scanner.nextInt();
+			
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
-			cal.printCalendar(2017, month);
+			cal.printCalendar(year, month);
 
 		}
 		System.out.println("bye~");
